f403280299194393d3caf4d3d6740a2f55551261
Dominik Dumas <dominikdumas@Dominiks-MacBook-Pro.local>
2019-03-06 10:53:30 -0500

    removed (comment) multiple lines

--- app.js
+++ app.js
@@ -4,33 +4,50 @@ const fs = require('fs');
 const hostname = '10.200.26.136';
 const port = 3000;
 
+function getKeyboard(response) {
+    fs.readFile('./public/keyboard.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
 let handleRequest = (request, response) => {
     
     console.log(request.url);
-    switch (request.method) {
-        case "GET":
-            if (request.url === "/") {
-                response.writeHead(200, { "Content-Type": "text/plain"});
-                response.write("Hello there at /");
-            } else if (request.url === "/k") {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                fs.readFile('./public/keyboard.html', null, function (error, data) {
-                    if (error) {
-                        response.writeHead(404);
-                        response.write('Whoops! File not Found!');
-                    } else {
-                        response.write(data);
-                    }
-                    response.end();
-                });
-            }
-            break;
-        case "POST":
-            break;
-        default:
-                break;
-    }
-    response.end();
+    
+    getKeyboard(response);
+    
+//    switch (request.method) {
+//        case "GET":
+//            if (request.url === "/") {
+//                response.writeHead(200, { "Content-Type": "text/plain"});
+//                response.write("Hello there at /");
+//            } else if (request.url === "/k") {
+//                response.writeHead(200, {'Content-Type': 'text/html'});
+//                fs.readFile('./public/keyboard.html', null, function (error, data) {
+//                    if (error) {
+//                        response.writeHead(404);
+//                        response.write('Whoops! File not Found!');
+//                    } else {
+//                        response.write(data);
+//                    }
+////                    response.end();
+//                });
+//            }
+//            break;
+//        case "POST":
+//            break;
+//        default:
+////            response.end();
+//            break;
+//    }
+//    response.end();
+    
 //    response.writeHead(200, {
 //        'Content-Type': 'text/html'
 //    });
