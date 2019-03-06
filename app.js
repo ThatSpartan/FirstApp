c132c2aff207b79be0fc15b53993de0ecbef977b
Dominik Dumas <dominikdumas@Dominiks-MacBook-Pro.local>
2019-03-05 20:37:07 -0500

    added keyboard && procfile

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+const fs = require('fs');
+
+const hostname = '10.200.26.136';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+let handleRequest = (request, response) => {
+    
+    
+    
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./public/keyboard.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+http.createServer(handleRequest).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running aft http://${hostname}:${port}/`);
-});
+let getHomePage = (request, response) => {
+    
+};
 
-const path = require('path');
+//const server = http.createServer((req, res) => {
+////    res.statusCode = 200;
+////    res.setHeader('Content-Type', 'text/plain');
+////    res.end('Hello, World!\n');
+//});
+//
+//server.listen(port, hostname, () => {
+//    console.log(`Server running at http://${hostname}:${port}/`);
+//});
 
-const myPath = path.basename(__filename);
+
+
+//const path = require('path');
+
+//const myPath = path.basename(__filename);
